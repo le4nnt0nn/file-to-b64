d476de9c554be7aaa3700d4ec67761bd2b90875c
Antonio Cortés <71550295+le4nnt0nn@users.noreply.github.com>
2024-10-27 22:35:45 +0100

    feat : add insert into database

--- Program.cs
+++ Program.cs
@@ -12,6 +12,9 @@ class Program
         // Leer y convertir el archivo a base64
         string base64String = ConvertFileToBase64(filePath);
 
+        // Guardar el resultado en SQL Server
+        SaveBase64ToDatabase("chispis.jpg", base64String);
+
         Console.WriteLine(base64String);
     }
 
@@ -22,5 +25,28 @@ class Program
 
         // Convertir a base64
         return Convert.ToBase64String(fileBytes);
+    }
+
+    static void SaveBase64ToDatabase(string fileName, string base64Content)
+    {
+        // Cadena de conexión a SQL Server
+        string connectionString = "Server=;Database=;User Id=;Password=;";
+
+        // Consulta de inserción
+        string query = "INSERT INTO ArchivosBase64 (NombreArchivo, Base64Contenido) VALUES (@FileName, @Base64Content)";
+
+        // Conectar y ejecutar la consulta
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        using (SqlCommand command = new SqlCommand(query, connection))
+        {
+            command.Parameters.AddWithValue("@FileName", fileName);
+            command.Parameters.AddWithValue("@Base64Content", base64Content);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        Console.WriteLine("Archivo guardado en la base de datos exitosamente.");
     }
 }
